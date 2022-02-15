152662e5de8e1f30710a2d276a51866df63851e3
micaela quijano <micaquijano04@gmail.com>
2022-02-14 23:50:50 -0300

    min fix

--- assets/java script/carrito.js
+++ assets/java script/carrito.js
@@ -1,26 +1,24 @@
-
 class Carrito {
-    productos = [];
-    agregarProducto(producto) {
-      let productoEncontrado = this.productos.findIndex(
-        (p) => p.id === producto.id
-      );
-      if (productoEncontrado !== -1) {
-        this.productos.splice(productoEncontrado, 1, producto);
-      } else {
-        this.productos.push(producto);
-      }
-    }
-    quitarProducto(producto) {
-      this.productos.slice(producto);
+  productos = [];
+  agregarProducto(producto) {
+    let productoEncontrado = this.productos.findIndex(
+      (p) => p.id === producto.id
+    );
+    if (productoEncontrado !== -1) {
+      this.productos.splice(productoEncontrado, 1, producto);
+    } else {
+      this.productos.push(producto);
     }
   }
+  quitarProducto(producto) {
+    this.productos.slice(producto);
+  }
+}
 
-  const carrito = new Carrito();
+const carrito = new Carrito();
 
-
-  function getCarritoHtml(carrito) {
-    let html = `<table class="table">
+function getCarritoHtml(carrito) {
+  let html = `<table class="table">
                 <thead>
                   <tr>
                     <th>#Id</th>
@@ -29,22 +27,22 @@ class Carrito {
                   </tr>
                 </thead>
                 <tbody>`;
-    carrito.productos.forEach((producto) => {
-      html += `<tr>
+  carrito.productos.forEach((producto) => {
+    html += `<tr>
                 <th>${producto.id}</th>
                 <td>${producto.nombre}</td>
                 <td>${producto.precio}</td>
               </tr>`;
-    });
-    html += `</tbody>
+  });
+  html += `</tbody>
           </table>`;
-    return html;
-  }
+  return html;
+}
 
-  function getCarritoInfo(carrito) {
-    const carritoHtml = getCarritoHtml(carrito);
-  
-    return ` <div class="modal fade" id="carrito" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+function getCarritoInfo(carrito) {
+  const carritoHtml = getCarritoHtml(carrito);
+
+  return ` 
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
@@ -58,13 +56,10 @@ class Carrito {
           <button type="button" class="btn btn-primary">Finalizar compra</button>
         </div>
       </div>
-    </div>
   </div>`;
-  }
-  
+}
 
-$(document).on("ready", function(){
-  const modal = getCarritoInfo(carrito)
-  $("#modalCarrito").html(modal)
-})
-  
+$(document).on("ready", function () {
+  const modal = getCarritoInfo(carrito);
+  $("#modalCarrito").html(modal);
+});
