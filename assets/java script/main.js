a8cedf1d53beb2b61f5f0f51151dcaa759f2ac00
micaela quijano <micaquijano04@gmail.com>
2022-02-15 19:23:49 -0300

    agregar producto

--- assets/java script/main.js
+++ assets/java script/main.js
@@ -1,3 +1,18 @@
+var brelettes = require('../datos/brelettes.json');
+var conjuntos = require('../datos/brelettes.json');
+var bombachas = require('../datos/bombachas.json');
+
+var todosLosProductos = [];
+brelettes.forEach(b => todosLosProductos.push(b))
+conjuntos.forEach(c => todosLosProductos.push(c))
+bombachas.forEach(b => todosLosProductos.push(b))
+
+function agregarProductoACarrito(id) {
+  console.log(id);
+  const producto = todosLosProductos.find(producto => producto.id === id);
+  carrito.agregarProducto(producto);
+}
+
 //ajax que obtiene el header
 $.ajax({
   type: "GET",
@@ -39,37 +54,39 @@ function obtenerProductos(_url) {
   }
 } */
 
-function agregarProductoACarrito(producto, id) {
-  console.log(producto);
-  carrito.agregarProducto(producto);
+
+function obtenerProductos(_url) {
+  $.ajax({
+    type: "GET",
+    url: _url,
+  }).done(function (data) {
+    let productosHtml = "";
+    data.forEach((producto) => {
+      productosHtml += getProductoHtml(producto);
+    });
+    $("#productos").html(productosHtml);
+  });
 }
+
+
+
 
 function getProductoHtml(producto) {
   return `<div class="card img-with-zoom col-4 m-0 p-0">
-
-
-  <img
-    src="${producto.img}"
-    class="card-img-top"
-    alt="..."
-  />
+  <img src="${producto.img}" class="card-img-top"/>
   <div class="card-body">
     <div class="card-title d-flex justify-content-around">
       <h1 class="me-auto">${producto.precio}</h1>
       <span class="badge bg-secondary fs-4 fit-height">${producto.id}</span>
     </div>
     <span class="card-text">${producto.nombre}</span>
-    <a
-  target="_blank"
-  class="text-white"
-  href="https://wa.me/+5491126721843?text=Hola,%20quisiera%20más%20info%20de%20el%20producto%20%23${producto.id}"
->
-<img src="./assets/imagenes/whatsapp.png" class="wsp-icono"
-    />
-</a>
+    <a target="_blank" class="text-white"
+      href="https://wa.me/+5491126721843?text=Hola,%20quisiera%20más%20info%20de%20el%20producto%20%23${producto.id}">
+      <img src="./assets/imagenes/whatsapp.png" class="wsp-icono"/>
+    </a>
     <button
     type="button"
-    onClick="agregarProductoACarrito(${producto}, ${producto.id}); $event.stopPropagation()"
+    onClick="agregarProductoACarrito(${producto.id}); $event.stopPropagation()"
     class="btn btn-primary"
   >Comprar</button>
   </div>
